9cccf3adfb5f398f7d8f466a238f007fc73f9f30
adhish2149 <62374112+adhish2149@users.noreply.github.com>
2023-05-16 23:59:01 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -6,8 +6,10 @@ from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.metrics import accuracy_score
 # DATA FOR PRED for diabetes
 data=pd.read_csv("diabetes.csv")
+#returns first five rows
 print(data.head())
 
+#excluding the last column as it is the target column
 X=data.iloc[:,:8]
 print(X.shape[1])
 
@@ -17,8 +19,14 @@ X=np.array(X)
 y=np.array(y)
 
 # Define the hyperparameters to search over
+'''
+n_estimators: The number of trees in the random forest ensemble.
+max_depth: The maximum depth of each decision tree in the random forest
+min_samples_split: minimum number of samples to split for a child nodes
+min_samples_leaf: minimun samples at the leaf of a descision tree 
+'''
 param_grid = {
-    'n_estimators': [50, 100, 200],
+     'n_estimators': [50, 100, 200],
     'max_depth': [3, 5, 10],
     'min_samples_split': [2, 5, 10],
     'min_samples_leaf': [1, 2, 4],
@@ -28,6 +36,10 @@ param_grid = {
 random_classifier = RandomForestClassifier()
 
 # Use GridSearchCV to find the best hyperparameters
+'''
+GridSearchCV: It is performs search over specified parameters
+cv: number of folds
+'''
 grid_search = GridSearchCV(estimator=random_classifier, param_grid=param_grid, cv=5)
 grid_search.fit(X, y.reshape(-1,))
 
